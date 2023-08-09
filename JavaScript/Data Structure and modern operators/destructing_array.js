1ab32350f7e46cda19b394f31fd687b4c2a9c92e
viralweybee <140693399+viralweybee@users.noreply.github.com>
2023-08-09 18:44:04 +0530

    Add files via upload

---
+++ JavaScript/Data Structure and modern operators/destructing_array.js
@@ -0,0 +1,14 @@
+const arr=[2,3,4]
+const a=arr[0]
+const b=arr[1]
+const c=arr[2]
+const [x,y,z]=arr
+console.log(x,y,z)
+const restaurant={
+    name: 'classico Italian',
+    location : 'Via Angelo Tavanti 23,Firenze ,Italy',
+    categories: ['Italian','Pizzeria','Vegeterian','Organic'],
+    starterMenu:['Foccasia','Bruschetta','Garlic','Bread','Capress Salad']
+}
+const [first,second]=restaurant.categories
+console.log(first,second)
